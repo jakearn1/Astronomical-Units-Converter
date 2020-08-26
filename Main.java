8f3ae8532a2674f0ecd36802c23d9ee3c74764f3
jakearn1 <69870578+jakearn1@users.noreply.github.com>
2020-08-26 12:13:13 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) 
+{
+    double au = 1.0;
+    System.out.println("The distance in Astronmical Units is " + au);
+    System.out.println("That distance in miles is " + au*9290000);
+    System.out.println("That distance in light years is " + au*0.000015813);
+    System.out.println("That distance in kilometers is " + au*149600000);
+}
+}
